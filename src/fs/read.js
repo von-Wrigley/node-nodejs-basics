186f2498f2c35a302ab7df2d224db7ddc09a5b1a
von-Wrigley <119867977+von-Wrigley@users.noreply.github.com>
2025-04-27 01:09:02 +0300

    Update read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,15 @@
+import fs from 'node:fs/promises';
+
+
 const read = async () => {
-    // Write your code here 
+   try {
+  
+    let data = await fs.readFile('src/fs/files/fileToRead.txt', 'utf8');
+	console.log(data);
+    
+   } catch (error) {
+    throw new Error('FS operation failed')
+   }
 };
 
 await read();
