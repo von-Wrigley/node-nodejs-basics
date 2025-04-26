eb72cf1ab973de043c81c6086063e812b3242085
von-Wrigley <119867977+von-Wrigley@users.noreply.github.com>
2025-04-27 00:18:39 +0300

    Update copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,22 @@
+import { access, constants } from 'fs';
+import fs from 'fs/promises';
+
 const copy = async () => {
-    // Write your code here 
+
+access('src/fs/files_copy', constants.F_OK, error => {
+ if(error){
+
+     fs.mkdir('src/fs/files_copy')
+     fs.cp('src/fs/files', 'src/fs/files_copy',  {recursive: true});
+
+ }
+ else{
+    throw new Error('FS operation failed')
+ }
+
+})
+    
+
 };
 
 await copy();
