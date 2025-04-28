94211234b3e8c0df0cb07e7582ca394eb025d054
von-Wrigley <119867977+von-Wrigley@users.noreply.github.com>
2025-04-28 19:39:04 +0300

    Update calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,20 @@
+import crypto from 'crypto'
+import fs from 'fs';
+import { join } from 'path';
+import { fileURLToPath } from "node:url";
+
 const calculateHash = async () => {
-    // Write your code here 
+
+   const dirname = fileURLToPath(new URL('.', import.meta.url));
+   const filePath = join(dirname, 'files/fileToCalculateHashFor.txt');
+   const str = fs.createReadStream(filePath)
+
+   str.on('data', data => {
+    const hash = crypto.createHash('sha256')
+    hash.update(data)
+    console.log(hash.digest('hex')) 
+   })
+   
 };
 
 await calculateHash();
