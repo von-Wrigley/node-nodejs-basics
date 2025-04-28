ae47e0c57a776ee06f776255b1f5815843a45e64
von-Wrigley <119867977+von-Wrigley@users.noreply.github.com>
2025-04-28 20:03:06 +0300

    Update delete.js
    rewrite path

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,19 +1,18 @@
-
+import { join } from 'path';
+import { fileURLToPath } from "node:url";
 import { access, unlink, constants } from 'fs/promises';
 
 const remove = async () => {
-    
+    const dirname = fileURLToPath(new URL('.', import.meta.url));
+    const oldPath = join(dirname, 'files/fileToRemove.txt');
 try {
 
-    await access('src/fs/files/fileToRemove.txt', constants.F_OK)
-    await unlink('src/fs/files/fileToRemove.txt')
+    await access(oldPath, constants.F_OK)
+    await unlink(oldPath)
     
 } catch (error) {
     throw new Error('FS operation failed')
 }
-
-
-
 };
 
 await remove();
