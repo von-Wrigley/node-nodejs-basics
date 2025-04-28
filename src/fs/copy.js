ac970c04ac6b954534c378893ec7773fea5bb46f
von-Wrigley <119867977+von-Wrigley@users.noreply.github.com>
2025-04-28 19:52:52 +0300

    Update copy.js
    rewrite paths

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,22 +1,24 @@
 import { access, constants } from 'fs';
 import fs from 'fs/promises';
+import { join } from 'path';
+import { fileURLToPath } from "node:url";
 
 const copy = async () => {
-
-access('src/fs/files_copy', constants.F_OK, error => {
+    const dirname = fileURLToPath(new URL('.', import.meta.url));
+    const oldPath = join(dirname, 'files');
+    const newPath = join(dirname, 'files_copy');
+   
+access(newPath, constants.F_OK, error => {
  if(error){
 
-     fs.mkdir('src/fs/files_copy')
-     fs.cp('src/fs/files', 'src/fs/files_copy',  {recursive: true});
-
+     fs.mkdir(newPath)
+     fs.cp(oldPath, newPath,  {recursive: true});
  }
  else{
     throw new Error('FS operation failed')
  }
 
 })
-    
-
 };
 
 await copy();
