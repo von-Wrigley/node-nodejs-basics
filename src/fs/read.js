e6dffa7e2a471d632d006d8e375d73425d460615
von-Wrigley <119867977+von-Wrigley@users.noreply.github.com>
2025-04-28 20:05:53 +0300

    Update read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,12 +1,13 @@
 import fs from 'node:fs/promises';
-
+import { join } from 'path';
+import { fileURLToPath } from "node:url";
 
 const read = async () => {
+   const dirname = fileURLToPath(new URL('.', import.meta.url));
+   const oldPath = join(dirname, 'files/fileToRead.txt');
    try {
-  
-    let data = await fs.readFile('src/fs/files/fileToRead.txt', 'utf8');
-	console.log(data);
-    
+    let data = await fs.readFile(oldPath, 'utf8');
+	console.log(data)
    } catch (error) {
     throw new Error('FS operation failed')
    }
