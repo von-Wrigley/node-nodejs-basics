bcd54afcd6b4d8ed80370a59262d66319f5f9c5c
von-Wrigley <119867977+von-Wrigley@users.noreply.github.com>
2025-04-28 19:19:31 +0300

    Update worker.js

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,22 @@
-// n should be received from main thread
+import { parentPort  } from 'node:worker_threads';
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+ parentPort.on('message', (num) => {
+    try {
+     let x = nthFibonacci(num)
+    parentPort.postMessage({
+        status: 'resolved',
+        data: x
+    })
+}
+        
+     catch (error) {
+        parentPort.postMessage({
+            status: 'error',
+            data: null
+        })        
+    }} )
 };
 
 sendResult();
