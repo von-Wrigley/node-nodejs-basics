9267a7f5affc445e5b37db495967c27cea8b506f
von-Wrigley <119867977+von-Wrigley@users.noreply.github.com>
2025-04-28 19:22:32 +0300

    Update main.js

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,33 @@
+import os from 'node:os'
+import { Worker } from 'node:worker_threads';
+import { fileURLToPath } from "node:url";
+import { join } from 'path';
+
 const performCalculations = async () => {
-    // Write your code here
+           const cores =  os.cpus().length
+        let arrNum =  []
+        let x =  []
+        const dirname = fileURLToPath(new URL('.', import.meta.url));
+        const filePath = join(dirname, 'worker.js');
+
+   for(let i = 0; i< cores; i++){
+         const myWorker = new Worker(filePath);
+         const num = 10 +i 
+         myWorker.postMessage(num)
+        x.push(  new Promise((resolve, reject) =>{
+            myWorker.on('message', (message) => {
+                arrNum.push(message) 
+                    resolve()     }
+                   ),   
+            myWorker.on('error', (message) => {
+                    arrNum.push(message)
+                            reject()
+                        } )}))
+        
+        }
+          
+          await   Promise.all(x);
+          console.log(arrNum)
 };
 
 await performCalculations();
