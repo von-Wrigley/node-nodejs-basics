2fce538571a08274ae893ccb322b64c6db779bf0
von-Wrigley <119867977+von-Wrigley@users.noreply.github.com>
2025-04-28 19:26:21 +0300

    Update read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,15 @@
-const read = async () => {
-    // Write your code here 
-};
+import fs from 'fs';
+import { join } from 'path';
+import { fileURLToPath } from "node:url";
 
-await read();
+async function readAndPrintFile() {
+    const dirname = fileURLToPath(new URL('.', import.meta.url));
+    const filePath = join(dirname, 'files/fileToRead.txt');
+    const fileStream = fs.createReadStream(filePath, 'utf8');
+ fileStream.on('data', chunk=> {
+  process.stdout.write(chunk, 'utf8')
+ })
+ fileStream.on('end', () => process.stdout.write('\n'));
+}
+
+await readAndPrintFile();
