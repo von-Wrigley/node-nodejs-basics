05ae3ab41ea033bdc3b1a8db374f2772a875d4f3
von-Wrigley <119867977+von-Wrigley@users.noreply.github.com>
2025-04-28 19:11:24 +0300

    Update compress.js

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,24 @@
+import { createGzip  } from 'node:zlib'
+import { fileURLToPath } from "node:url";
+import {
+    createReadStream,
+    createWriteStream,
+  } from 'node:fs';
+  import { pipeline } from 'node:stream';
+  import process from 'node:process';
+  import { join } from 'path';
 const compress = async () => {
-    // Write your code here 
+    const dirname = fileURLToPath(new URL('.', import.meta.url));
+    const filePath = join(dirname, 'files/fileToCompress.txt');
+    const newfilePath = join(dirname, 'files/archive.gz');
+        const gzip  = createGzip()
+        const source = createReadStream(filePath);
+        const destination = createWriteStream(newfilePath);
+pipeline(source, gzip, destination, (err) => {
+  if (err) {
+    console.error('An error occurred:', err);
+    process.exitCode = 1;
+  }
+});
 };
-
 await compress();
