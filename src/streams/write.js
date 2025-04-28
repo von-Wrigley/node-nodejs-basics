14529d2442a361f6b52b8d183558beeefb8b501a
von-Wrigley <119867977+von-Wrigley@users.noreply.github.com>
2025-04-28 19:28:11 +0300

    Update write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,19 @@
+import fs from 'fs';
+import { join } from 'path';
+import { fileURLToPath } from "node:url";
+
 const write = async () => {
-    // Write your code here 
+    const dirname = fileURLToPath(new URL('.', import.meta.url));
+    const filePath = join(dirname, 'files/fileToWrite.txt');
+    const writeableStream = fs.createWriteStream(filePath, 'utf-8');
+    process.stdin.on('data', data=> {
+        if(data==='exit'){
+            process.exit()
+        }
+else {
+    writeableStream.write(data)
+}
+    })
 };
 
 await write();
