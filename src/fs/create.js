f41f9a0bdc8cdb0f687a87bf686cd51b762adcbe
von-Wrigley <119867977+von-Wrigley@users.noreply.github.com>
2025-04-28 19:47:55 +0300

    Update create.js
    rewrite path

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,13 +1,13 @@
 import fs from 'fs';
-import path from 'node:path';
-
+import { join } from 'path';
+import { fileURLToPath } from "node:url";
 
 const create = async () => {
- const x = path.join('src/fs/files', 'fresh.txt')
-  try {
-    
-    await fs.promises.writeFile(x, 'I am fresh and young', { flag: 'wx' })
-
+ const dirname = fileURLToPath(new URL('.', import.meta.url));
+ const filePath = join(dirname, 'files/fresh.txt');
+  
+ try {
+    await fs.promises.writeFile(filePath, 'I am fresh and young', { flag: 'wx' })
   } catch (error) {
     throw new Error('FS operation failed')
     
