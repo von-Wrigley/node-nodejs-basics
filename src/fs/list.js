6ed3ff2550b564bf281b05f323c21b0be0a89fc0
von-Wrigley <119867977+von-Wrigley@users.noreply.github.com>
2025-04-28 20:04:17 +0300

    Update list.js
    rewrite path

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,11 +1,13 @@
 import { readdir } from 'node:fs/promises';
+import { join } from 'path';
+import { fileURLToPath } from "node:url";
 
 const list = async () => {
 
-
+  const dirname = fileURLToPath(new URL('.', import.meta.url));
+  const oldPath = join(dirname, 'files');
     try {
-       
-        const contents = await readdir("src/fs/files");
+        const contents = await readdir(oldPath);
         console.log(contents);
       } catch (err) {
         throw new Error('FS operation failed')
